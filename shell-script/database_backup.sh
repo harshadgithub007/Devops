e45c414efdbcbc88e06a67993e96e8e9a5420957
Harshad <153172402+harshadgithub007@users.noreply.github.com>
2023-12-22 21:32:10 +0530

    Update database_backup.sh

--- shell-script/database_backup.sh
+++ shell-script/database_backup.sh
@@ -2,7 +2,7 @@
 
 # script to take backup of mysql and send to se bucket
 
-DB_USER="root"
+DB_USER="not root user remember"
 DB_PASSWORD="123"
 DB_NAME="devops"
 
@@ -31,3 +31,10 @@ then
 else
         echo "backup failed..........!!!"
 fi
+
+: '
+what we need to write in script to take the backup:-
+1. database user ( use coustom user " no root user ", database user's password and database name 
+2. we have to create backup directory ------ and give its path here
+3. Location where we send our backup file like AWS S3 Bucket
+4. write command to delete file from our server after backup is done.
