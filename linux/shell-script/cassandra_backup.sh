b5eae04938366c9c48ca35d4563b5ec99ffa566c
Harshad <153172402+harshadgithub007@users.noreply.github.com>
2024-01-16 12:14:04 +0530

    Update cassandra_backup.sh

--- linux/shell-script/cassandra_backup.sh
+++ linux/shell-script/cassandra_backup.sh
@@ -1,32 +1,30 @@
-#! /usr/bin/bash
+#! /bin/bash
+
 
 HOST="localhost"
-KEYSPACE="devops" # Database name
-SNAPSHOT="cassandra_backup"
-SNAPSHOT_DIR="/root/cassandra_backup"
+USER="harshad"
+PASS="1234"
+KEYSPACE="database_name"
+SNAPSHOT="database_$(date +%Y%m%d_%H%M%S)"
+SNAPSHOT_DIR="/home/cassandra"
+LOCATION="my_s3_bucket"
 
-LOCATION="my_bucket_007"
+# taking backup:-
 
-# Taking backup:
-nodetool snapshot -t "$SNAPSHOT" "$SNAPSHOT_DIR/$KEYSPACE"
+nodetool snapshot -u "$USER" -p "$PASS" -t "$SNAPSHOT_DIR"/"$SNAPSHOT" "$KEYSPACE"   #here backup is being created by the name of "$SNAPSHOT" and storing in the directory named "$SNAPSHOT_DIR"  
 
-# Check if nodetool snapshot was successful
-if [ $? -ne 0 ]; then
-    echo "Nodetool snapshot failed..."
-    exit 1
+if [ $? -ne 0 ]
+then
+    echo "taking backup failed"
 fi
 
-# Sending to S3:
-aws s3 cp "$SNAPSHOT_DIR/$KEYSPACE" s3://"$LOCATION"/
+# sending backup to s3:-
 
-# Check if AWS S3 upload was successful
-if [ $? -ne 0 ]; then
-    echo "AWS S3 upload failed..."
-    exit 1
+aws s3 cp "$SNAPSHOT_DIR"/"$SNAPSHOT" s3://"$LOCATION"/
+
+if [ $? -ne 0 ]
+then    
+    echo "sending backup to location is failed..."
 fi
 
-# Remove from local:
-rm -rf "$SNAPSHOT_DIR/$KEYSPACE"
 
-# Confirm completion
-echo "Backup completed successfully."
